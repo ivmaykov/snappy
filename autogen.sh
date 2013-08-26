8e099cd8e15078d4a11c1a5d4fbf4429ccea4008
Ilya Maykov <ivmaykov@gmail.com>
2013-08-26 15:49:19 -0700

    Make autogen.sh work on OS X if it has glibtoolize instead of libtoolize

--- autogen.sh
+++ autogen.sh
@@ -2,6 +2,13 @@
 rm -rf autom4te.cache
 aclocal -I m4
 autoheader
-libtoolize --copy
+if type libtoolize > /dev/null 2>&1; then
+  libtoolize --copy
+elif type glibtoolize > /dev/null 2>&1; then
+  glibtoolize --copy
+else
+  echo "ERROR: Could not find either libtoolize or glibtoolize" >&2;
+  exit 1;
+fi
 automake --add-missing --copy
 autoconf
